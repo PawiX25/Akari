8990045ff5cf96d5e3cd147479175228cc7b4539
PawiX25 <81538700+PawiX25@users.noreply.github.com>
2023-10-31 17:33:52 +0100

    prxmain: Refactor module start and stop functions for readability and maintainability

--- Akari/prxmain.cpp
+++ Akari/prxmain.cpp
@@ -20,67 +20,77 @@ SYS_MODULE_STOP(module_stop);
 
 sys_ppu_thread_t gMainPpuThreadId = SYS_PPU_THREAD_ID_INVALID;
 
+// Start of the module
 CDECL_BEGIN
 int module_start(size_t args, const void* argp)
 {
-	sys_ppu_thread_create(&gMainPpuThreadId, [](uint64_t arg) -> void
-	{
-		do Timers::Sleep(1000); while (!paf::View::Find("explore_plugin"));
+    // Create a new thread for the module
+    sys_ppu_thread_create(&gMainPpuThreadId, [](uint64_t arg) -> void
+    {
+        // Wait until the explore_plugin is found
+        do Timers::Sleep(1000); while (!paf::View::Find("explore_plugin"));
 
-		stdc::printf(BUILD_STRING "\n");
+        stdc::printf(BUILD_STRING "\n");
 
-		g_Input = Input();
-		g_Render = Render();
-		g_Helpers = Helpers();
+        // Initialize the global objects
+        g_Input = Input();
+        g_Render = Render();
+        g_Helpers = Helpers();
 
-		g_Config = Config();
-		g_Menu = Menu(MainSubmenu);
-		g_Overlay = Overlay();
+        g_Config = Config();
+        g_Menu = Menu(MainSubmenu);
+        g_Overlay = Overlay();
 
-		InstallHooks();
+        // Install the hooks
+        InstallHooks();
 
-		sys_ppu_thread_exit(0);
-	},
-		0, 1069, 1536, SYS_PPU_THREAD_CREATE_JOINABLE, "Akari::module_start");
+        sys_ppu_thread_exit(0);
+    },
+    0, 1069, 1536, SYS_PPU_THREAD_CREATE_JOINABLE, "Akari::module_start");
 
-	Syscall::_sys_ppu_thread_exit(0);
-	return 0;
+    Syscall::_sys_ppu_thread_exit(0);
+    return 0;
 }
 
+// Stop of the module
 int module_stop(size_t args, const void* argp)
 {
-	sys_ppu_thread_t stopPpuThreadId;
-	int ret = sys_ppu_thread_create(&stopPpuThreadId, [](uint64_t arg) -> void
-	{
-		RemoveHooks();
+    sys_ppu_thread_t stopPpuThreadId;
+    int ret = sys_ppu_thread_create(&stopPpuThreadId, [](uint64_t arg) -> void
+    {
+        // Remove the hooks
+        RemoveHooks();
 
-		g_Overlay.OnShutdown();
-		g_Config.Save();
-		g_Render.DestroyWidgets();
+        // Handle shutdown for overlay
+        g_Overlay.OnShutdown();
 
-		sys_ppu_thread_yield();
-		Timers::Sleep(1337);
+        // Save the configuration and destroy the widgets
+        g_Config.Save();
+        g_Render.DestroyWidgets();
 
-		if (gMainPpuThreadId != SYS_PPU_THREAD_ID_INVALID)
-		{
-			uint64_t exitCode;
-			sys_ppu_thread_join(gMainPpuThreadId, &exitCode);
-		}
+        sys_ppu_thread_yield();
+        Timers::Sleep(1337);
 
-		sys_ppu_thread_exit(0);
-	},
-		0, 2814, 1024, SYS_PPU_THREAD_CREATE_JOINABLE, "Akari::module_stop");
+        // Join the main thread if it's valid
+        if (gMainPpuThreadId != SYS_PPU_THREAD_ID_INVALID)
+        {
+            uint64_t exitCode;
+            sys_ppu_thread_join(gMainPpuThreadId, &exitCode);
+        }
 
-	if (ret == 0)
-	{
-		uint64_t exitCode;
-		sys_ppu_thread_join(stopPpuThreadId, &exitCode);
-	}
+        sys_ppu_thread_exit(0);
+    },
+    0, 2814, 1024, SYS_PPU_THREAD_CREATE_JOINABLE, "Akari::module_stop");
 
-	Timers::Sleep(5);
+    // Join the stop thread if it's valid
+    if (stopPpuThreadId != SYS_PPU_THREAD_ID_INVALID)
+    {
+        uint64_t exitCode;
+        sys_ppu_thread_join(stopPpuThreadId, &exitCode);
+    }
 
-	Syscall::finalize_module();
-	Syscall::_sys_ppu_thread_exit(0);
-	return 0;
+    // Exit the current thread
+    Syscall::_sys_ppu_thread_exit(0);
+    return 0;
 }
 CDECL_END
